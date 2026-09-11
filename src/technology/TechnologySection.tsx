7c30bb5a990e729722d6663dc34188180495119e
ijannathalima-ux <ijannathalima@gmail.com>
2026-09-11 23:04:41 +0600

    Update technology section text

--- src/technology/TechnologySection.tsx
+++ src/technology/TechnologySection.tsx
@@ -19,7 +19,7 @@ const TechnologySection = ({ technologiesDataPromise }: TechnologyProps) => {
         <div>
             <div className="container mx-auto">
                 <h2 className="text-4xl font-bold mb-4">Explore the <span className="bg-linear-to-r from-orange-500 via-pink-500 to-purple-600 bg-clip-text text-transparent">Technologies</span></h2>
-                <p className="text-[#64748B]">Pick one technology per category to build your ideal stack.</p>
+                <p className="text-[#64748B] text-[19px] font-medium">Pick one technology per category to build your ideal stack.</p>
             </div>
 
 
